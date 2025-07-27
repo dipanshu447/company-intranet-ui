11eda1cbca1ea433aad4542f9a5525ac5f0e72f1
dipanshu447 <dipanshusahu447@gmail.com>
2025-07-27 12:21:17 +0530

    add: added functionality to date and time

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,11 +1,22 @@
 export default function Welcome() {
     const currentDate = new Date();
-    console.log(currentDate)
+    const time = currentDate.toLocaleTimeString('en-US', {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true
+    });
+    const date = currentDate.toLocaleDateString('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    })
+
     return (
         <div className="dark:bg-[#202a3b] shadow-md overflow-visible box-border flex flex-col items-center py-6 gap-2 rounded-2xl">
             <div className="text-white font-bold text-2xl mb-1">Welcome back, Sarah!</div>
-            <span className="text-white font-semibold text-4xl">10:42 <small>AM</small></span>
-            <span className="text-[#9da2af]">Tuesday, July 26, 2025</span>
+            <span className="text-white font-semibold text-3xl">{time}</span>
+            <span className="text-[#9da2af]">{date}</span>
         </div>
     )
 }
